5ba642e0a9b9a3fdb28e257744ee98f0f6a0a021
Yash Khare <yashsja@gmail.com>
2020-09-25 14:04:50 +0530

    feat: Image labelling with text to speech

---
+++ android/app/src/main/kotlin/com/team4/helping_hands/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.team4.helping_hands
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/live_labelling/bndbox.dart
@@ -0,0 +1,134 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:math' as math;
+import 'models.dart';
+
+class BndBox extends StatefulWidget {
+  final List<dynamic> results;
+  final int previewH;
+  final int previewW;
+  final double screenH;
+  final double screenW;
+  final String model;
+
+  BndBox(this.results, this.previewH, this.previewW, this.screenH, this.screenW,
+      this.model);
+
+  @override
+  _BndBox createState() => new _BndBox();
+}
+
+class _BndBox extends State<BndBox> {
+  final FlutterTts flutterTts = FlutterTts();
+  var _timer;
+
+  @override
+  void initState() {
+    _timer = new Timer.periodic(Duration(milliseconds: 800),
+            (Timer timer) => _speak());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  String speak;
+  String prevString;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _renderBoxes() {
+      return widget.results.map((re) {
+        var _x = re["rect"]["x"];
+        var _w = re["rect"]["w"];
+        var _y = re["rect"]["y"];
+        var _h = re["rect"]["h"];
+        var scaleW, scaleH, x, y, w, h;
+
+        if (widget.screenH / widget.screenW > widget.previewH / widget.previewW) {
+          scaleW = widget.screenH / widget.previewH * widget.previewW;
+          scaleH = widget.screenH;
+          var difW = (scaleW - widget.screenW) / scaleW;
+          x = (_x - difW / 2) * scaleW;
+          w = _w * scaleW;
+          if (_x < difW / 2) w -= (difW / 2 - _x) * scaleW;
+          y = _y * scaleH;
+          h = _h * scaleH;
+        } else {
+          scaleH = widget.screenW / widget.previewW * widget.previewH;
+          scaleW = widget.screenW;
+          var difH = (scaleH - widget.screenH) / scaleH;
+          x = _x * scaleW;
+          w = _w * scaleW;
+          y = (_y - difH / 2) * scaleH;
+          h = _h * scaleH;
+          if (_y < difH / 2) h -= (difH / 2 - _y) * scaleH;
+        }
+
+        speak = re["detectedClass"];
+
+        return Positioned(
+          left: math.max(0, x),
+          top: math.max(0, y),
+          width: w,
+          height: h,
+          child: Container(
+            padding: EdgeInsets.only(top: 5.0, left: 5.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color.fromRGBO(37, 213, 253, 1.0),
+                width: 3.0,
+              ),
+            ),
+            child: Text(
+              "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+              style: TextStyle(
+                color: Color.fromRGBO(37, 213, 253, 1.0),
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        );
+      }).toList();
+    }
+
+    List<Widget> _renderStrings() {
+      double offset = -10;
+      return widget.results.map((re) {
+        offset = offset + 14;
+        print(re["label"]);
+        return Positioned(
+          left: 10,
+          top: offset,
+          width: widget.screenW,
+          height: widget.screenH,
+          child: Text(
+            "${re["label"]} ${(re["confidence"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              color: Color.fromRGBO(37, 213, 253, 1.0),
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      }).toList();
+    }
+
+    return Stack(
+      children: widget.model == mobilenet
+          ? _renderStrings()
+          :  _renderBoxes(),
+    );
+  }
+
+  Future _speak() async{
+    speak != null && speak != prevString ? await flutterTts.speak(speak) : null ;
+    prevString = speak;
+  }
+}
