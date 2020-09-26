6e91efbc259430205b5c74ef7fe03d8bddaa904e
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2020-09-27 02:22:09 +0530

    feat: Currency detection with text to speech

--- lib/currency_detection/currency.dart
+++ lib/currency_detection/currency.dart
@@ -1,13 +1,101 @@
-import 'package:flutter/material.dart';
 
-class Curr extends StatefulWidget {
+import 'dart:io';
+import 'package:tflite/tflite.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+import '../home.dart';
+
+
+class CurrPage extends StatefulWidget {
+
+  File currImage;
+  CurrPage({this.currImage});
+
+
   @override
-  _CurrState createState() => _CurrState();
+  _CurrPageState createState() => _CurrPageState(this.currImage);
 }
 
-class _CurrState extends State<Curr> {
+class _CurrPageState extends State<CurrPage> {
+
+  File currImage;
+  _CurrPageState(this.currImage);
+  List _output;
+  final FlutterTts flutterTts = FlutterTts();
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      // setState(() {});
+    });
+    speakCurrencyValue();
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  classifyCurrency(File image) async{
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 7,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5
+    );
+    print("This is the $output");
+    print("This is the ${output[0]['label']}");
+    dynamic label = output[0]['label'];
+    print(label.runtimeType);
+    _speak(label);
+    setState(() {
+      _output = output;
+    }
+
+
+    );
+  }
+  loadModel() async {
+    await Tflite.loadModel(
+      model: 'assets/cash_model_unquant.tflite',
+      labels: 'assets/cash_labels.txt',
+    );
+  }
+
+  speakCurrencyValue() {
+    classifyCurrency(currImage);
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+        appBar: AppBar(
+        title: Text('Currency Identification'),
+          backgroundColor: Hexcolor('b56576'),
+    ),
+        body: Center(
+          child: SizedBox.expand(
+            child: GestureDetector(
+              onTap: () => speakCurrencyValue(),
+              onDoubleTap: () {
+                
+              },
+              child: Image.file(currImage),
+        )
+      )
+    )
+    );
   }
+
+  Future _speak(String output) async{
+    await flutterTts.speak(output);
+  }
+
 }
