18d6fd9b89f221330e57292c794b29bee06da0f0
Yash Khare <yashsja@gmail.com>
2020-10-02 00:44:50 +0530

    feat: Display splash screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
+import 'home.dart';
 import 'main.dart';
 
 class MySplash extends StatefulWidget {
@@ -12,17 +13,12 @@ class _MySplashState extends State<MySplash> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 2,
-      navigateAfterSeconds: MyApp(),
-      backgroundColor: Colors.blue,
-      title: Text(
-        'Helping Hand',
-        style: TextStyle(
-            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
-      ),
+      seconds: 3,
+      navigateAfterSeconds: new HomePage(cameras),
+      backgroundColor: Colors.white,
       photoSize: 150,
-      loaderColor: Colors.white,
-      image: Image.asset('assets/images/icon.png'),
+      loaderColor: Colors.blueAccent,
+      image: Image.asset('assets/icon.jpg'),
     );
   }
 }
